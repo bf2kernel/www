230eff7e37cceb1fe692c08ac6bbcc7ce2429684
bf2kernel <bf2kernel@gmail.com>
2017-09-20 22:57:31 +0300

    all
    all

---
+++ en/js/main.js
@@ -0,0 +1,17 @@
+﻿// Header
+$(function () {
+    var shrinkHeader = 30;
+    var navbar = $(".navbar");
+    $(window).scroll(function () {
+        var scroll = getCurrentScroll();
+        if (scroll >= shrinkHeader) {
+            navbar.addClass("shrink");
+        }
+        else {
+            navbar.removeClass("shrink");
+        }
+    });
+    function getCurrentScroll() {
+        return window.pageYOffset || document.documentElement.scrollTop;
+    }
+});
---
+++ ru/config.inc.php
@@ -0,0 +1,5 @@
+<?php
+$ROOT = 'http://localhost/en/images/monitoring/';
+$GAMESERVER = '192.168.0.102'; // Для монитора
+$GAMEPORT = '29900'; // Для монитора
+?>
